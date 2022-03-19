3dbc1e8e26a9252de5c6ae5dd4f16db8ac1e1228
Subhashree naik <subhashree.naik@gmail.com>
2022-03-19 08:51:16 +0530

    Welcome to Hotel Reservation System

---
+++ src/HotelReservationSystem.java
@@ -0,0 +1,9 @@
+
+public class HotelReservationSystem {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.println("Welcome to Hotel Reservation System");
+	}
+
+}
